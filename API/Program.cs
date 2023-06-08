c4e3a9485600a11691e061f7107fb0930e264e71
jackusername1 <j4073405@gmail.com>
2023-06-08 16:51:18 +0330

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -13,12 +13,17 @@ builder.Services.AddDbContext<DataContext>(opt=>
 }
 );
 
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-
+app.UseCors(x => x
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+            
 app.MapControllers();
 
 app.Run();
